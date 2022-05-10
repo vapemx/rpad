2cf7410d4e0cb4e15d30dccb3bb6998ef6c01422
vapemx <elias841@hotmail.com>
2022-05-09 20:29:10 -0500

    fix main.py

--- main.py
+++ main.py
@@ -3,42 +3,46 @@ import re
 import getWeb
 
 
-def getLinks():
-    links = []
-
+def getLinks(links):
     with open("links.txt") as fileLinks:
         lines = fileLinks.readlines()
         for line in lines:
-            if line == "\n":
+            if line in links:
                 continue
-            else:
-                links.append(line)
-    print(links)
+            line = line.split("\n")
+            links.append(line[0])
 
     return links
 
 
-def addLinks():
-    pattern = "([a-z]+.[a-zA-Z0-9]+.[a-z]{2,}/*)"
-    newLink = input("Escriba el link a agregar: ")
+def addLinks(links):
+    pattern = "(http[s]*://[a-z]+.[a-zA-Z0-9]+.[a-z]{2,}/*)"
+    newLink = input("Escriba el link a agregar con prefijo http al inicio: ")
     check = re.findall(pattern, newLink)
     if not check:
         print("Formato de link erróneo. ")
         addLinks()
     else:
-        with open("links.txt", "w") as fileLinks:
-            fileLinks.write("\n".join(newLink))
+        with open("links.txt", "a") as fileLinks:
+            fileLinks.write("\n"+newLink)
 
-    return getLinks()
+    return getLinks(links)
 
 
 def main():
-    links = getLinks()
+    links = []
+    links = getLinks(links)
     print("Esto son los link a utilizar: ")
+    print("\n")
     print(links)
+    print("\n")
     x = input("¿Desea agregar[s/N]?: ")
     if x == "S" or x == "s":
-        links = addLinks()
+        links = addLinks(links)
+        print("Esto son los link a utilizar: ")
+        print("\n")
+        print(links)
+
 
     soups = []
     linkDB = {}
@@ -49,7 +53,6 @@ def main():
 
     for i in range(len(links)):
         linkDB[links[i]] = soups[i]
-    print(linkDB)
 
 
 
