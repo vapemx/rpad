0ca39877439bf6f9b6879e6a3d2d99a4d95e4a0f
Yaayoo15 <105295599+Yaayoo15@users.noreply.github.com>
2022-05-14 19:38:24 -0500

    ACTUALIZACION WEATHER
    LE AGREGUE COMENTARIOS PARA QUE PUEDA ENTENDERSE

--- weather.py
+++ weather.py
@@ -3,56 +3,68 @@ Se va a obtener los 3 eventos siguientes y su clima.
 El problema está en que a veces entre el primer evento y el tercero es un més de diferencia.
 Hay que buscar solución a eso.
 '''
-
+#Se importan las librerias necesarias para generar nuestra API y obtener la información climatológica
 import requests
 import json
 from datetime import datetime
 
-listakey = []
-listavalue = []
-listadia = []
 
-Latitud = "25.767"
-Longitud = "-100.200"
+#Se crean las listas donde se irán almacenando las temperaturas de los días elegidos
+Momentos = []
+Temperaturas = []
+Dias = []
+
+#Nuestra ID del Api de OpenWeather, generada automaticamente por la plataforma
 appid = "a3a7114576a3b54a0a02aee0bb777dea"
-page=requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + Latitud + "&lon=" + Longitud + "&exclude=minutely,hourly&units=metric&appid=" + appid)
+
+#Generamos el link de nuestra ciudad y aplicamos el método request.get para hacer la solicitud http a OpenWeather
+page=requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat=41.389&lon=2.159&exclude=minutely,hourly&units=metric&appid=a3a7114576a3b54a0a02aee0bb777dea")
 print()
+
+#Aplicamos page.status_code para que podamos apreciar que código de respuesta obtenemos de nuestra solicitud
+#En nuestro caso buscamos que sea código 200 el cual representa que todo está en orden 
 print(f"The code of the request is: {page.status_code}")
 
+#Convertimos la cadena JSON obtenida en un diccionario de Python utilizando el método json.loads()
 weatherData = json.loads(page.content)
 
+#Buscaremos en todo el diccionario obtenido en el paso anterior
 for x, y in weatherData.items():
+    #Buscamos donde comienza la información de temperaturas de cada día, esto se sabe pues 
+    #Cada día diferente inicia con la clave "daily"
     if x == "daily":
-        for i in y[0:1]:
+        
+        #Damos el rango para poder imprimir los días que buscamos, en este caso buscamos del 20 al 22 de Mayo
+        for i in y[5:10]:
             fecha = int(i['dt'])
-            dia = datetime.utcfromtimestamp(fecha).strftime("%d-%m-%Y")
-            listadia.append(dia)
+            #El metodo datetime obtiene la fecha actual, y el metodo strftime le dará formato a dicha fecha, pues 
+            #Convierte una tupla generada en una cadena, que será nuestra fecha en formato aaaa/mm/dd
+            dia = datetime.utcfromtimestamp(fecha).strftime("%Y-%m-%d")
+            Dias.append(dia)
+            #Key representa la parte del día que se esta buscando, ejemplo: Morning, Evening, Night, por eso 
+            #se añadiran a la lista listakey que teniamos al principio, la cual contendrá dichas etiquetas 
+            #que serán vaciadas en el excel más adelante
             for key in i['temp']:
-                listakey.append(key)
-                listavalue.append(i['temp']['day'])
-                listavalue.append(i['temp']['eve'])
-                listavalue.append(i['temp']['night'])
-                listavalue.append(i['temp']['min'])
-                listavalue.append(i['temp']['max'])
-
-Datos= listavalue
-Resultado = []
-for repetido in Datos:
-    if repetido not in Resultado:
-        Resultado.append(repetido)
-
-print("                                         Dia   Tarde   Noche  MINIMA  MAXIMA")
-print(f"Las temperaturas para el 13 de Mayo son:{Resultado}")
-print()
-
-
-'''
-ESTO LO REVISARE DESPUES PORQUE IMPRIME LAS LISTAS MUCHISIMAS VECES (ATTE YAHIR)
-
-print(listakey)
-print(listavalue)
-print(listadia)
-input()
-'''
-
+                Momentos.append(key)
                 
+                #Se comienza a llenar nuestra lista de temperaturas
+
+                #Se busca las que coincidan con el día y se añaden a Temperaturas
+                Temperaturas.append(i['temp']['day'])
+                #Se busca las que coincidan con la tarde y se añaden a Temperaturas
+                Temperaturas.append(i['temp']['eve'])
+                #Se busca las que coincidan con la noche y se añaden a Temperaturas
+                Temperaturas.append(i['temp']['night'])
+                #Se busca las que coincidan con la minima y se añaden a Temperaturas
+                Temperaturas.append(i['temp']['min'])
+                #Se busca las que coincidan con la maxima y se añaden a Temperaturas
+                Temperaturas.append(i['temp']['max'])
+
+#Se imprimen los resultados
+print(f"El patron de resultados es el siguiente: {Momentos}")
+print("---------------")
+print(Temperaturas)
+print("---------------")
+print(f"Los días obtenidos son:  {Dias}")
+print("---------------")
+input()
